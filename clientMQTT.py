843c6aaf952d8073fd3aa69c4c15a8d210645ca8
test <test@213-99.iutbeziers.fr>
2018-12-14 15:23:18 +0100

    client MQTT

---
+++ clientMQTT.py
@@ -0,0 +1,10 @@
+import time
+import paho.mqtt.client as paho
+broker="10.202.0.136"
+	
+
+client= paho.Client()
+client.connect(broker)
+client.publish("serv/bdd","on")#publish
+#client.disconnect()
+client.loop_forever() 
